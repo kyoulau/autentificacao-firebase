31eb78e91efa6fa47d80d4cab1d033f4aa107711
kyoulau <laucosan05@gmail.com>
2024-03-24 01:33:46 -0300

    alteracao final autenticacao

--- autenticacao.py
+++ autenticacao.py
@@ -1,39 +1,91 @@
-#imports necessários
-
 import pyrebase
 import getpass
 
-firebase_config = {
-    "apiKey": "AIzaSyCYkrOD_XI2UGZq-SnIu5LI5xwUxDnzGL4",
-    "authDomain": "laura-a1062.firebaseapp.com",
-    "projectId": "laura-a1062",
-    "storageBucket": "laura-a1062.appspot.com",
-    "messagingSenderId": "802782163320",
-    "appId": "1:802782163320:web:285afe154f9508dcc4588d",
-    "measurementId": "G-MTH1Y1Y39D",
-    "databaseURL": "",
+attemps_number = 0
+
+firebaseConfig = {
+    'apiKey': "AIzaSyBvUVuJFpiuiqumI1-La8u0oBEKYlBjlnQ",
+    'authDomain': "projeto-de-teste-14d6b.firebaseapp.com",
+    'projectId': "projeto-de-teste-14d6b",
+    'storageBucket': "projeto-de-teste-14d6b.appspot.com",
+    'messagingSenderId': "1079945923254",
+    'appId': "1:1079945923254:web:208683c3ca5193f8f23417",
+    'measurementId': "G-ZKJQ3776KY",
+    "databaseURL": ""
 }
 
-#The sign_in_with_email_and_password() method will return user data including a token you can use to adhere to security rules.
-#Each of the following methods accepts a user token: get(), push(), set(), update(), remove() and stream().
-
 #iniciando projeto
-firebase = pyrebase.initialize_app(firebase_config)
+firebase = pyrebase.initialize_app(firebaseConfig)
 
 #autentico no firebase
 auth = firebase.auth()
 
-#criacao novo usuario
-username = input("Digite o nome do seu usuario:")
-
-#import de getpass
-password = getpass.getpass("Digite a senha")
+#criacao novo usuario com import de getpass
+def create_user():
+    print("CRIANDO USUÁRIO")
+    username = input("Digite o nome do seu usuario:")
+    password = getpass.getpass("Digite a senha")
+    try:
+        token = auth.create_user_with_email_and_password(username,password)
+        print("Cadastro feito com sucesso")
+        print(token)
+    except Exception as e:
+        print("Email já existe!")
+    return
 
 #autenticar usuario
-try:
-    token = auth.sign_in_with_email_and_password("dueltmp+t7m2l@gmail.com","123456")
-    print("foi")
-except Exception as e:
-    print("Senha invalida ou login errado ")
+def login():
+    print("LOGIN USUÁRIO")
+    username = input("Digite o nome do seu usuario:")
+    password = getpass.getpass("Digite a senha")
+    try:
+        token = auth.sign_in_with_email_and_password(username,password)
+        print("Login realizado com sucesso")
+        print(token)
+    except Exception as e:
+        print("Email ou senha inválidos")
+    return
+    
 
 #verificar email
+def verification_email():
+    username = input("Digite nome de usuário:")
+    password = getpass.getpass("Digite a senha para verificação")
+    try:
+        user = auth.sign_in_with_email_and_password(username,password)
+        auth.send_email_verification(user['idToken'])
+        print("Email de verificação enviado!")
+    except Exception as e:
+        print(f"algo deu errado {e}")
+    return
+
+#redefinir senha
+def password_reset():
+    username = input("Digite nome de usuário:")
+    try:
+        token = auth.send_password_reset_email(username)
+    except Exception as e:
+        print("Erro ao redefinir a senha") 
+
+while True:
+    attemps_number+= 1
+    if attemps_number == 6:
+            print("NUMERO DE TENTATIVAS FINALIZADAS")
+            break
+    pergunta = input("voce é um novo usuario?[S/N]")
+    print(f"tentativa n {attemps_number}")
+    if pergunta == "N":
+        login()
+        pergunta_dois = input("Deseja redefinir sua senha?[S/N]")
+        if pergunta_dois == "S":
+            password_reset()
+        else:
+            continue     
+        pergunta_tres = input("Deseja verificar seu email ?[S/N]")
+        if pergunta_tres == "S":
+            verification_email()
+        else:
+            continue
+    else:
+        create_user()
+        
---
+++ whileteste.py
@@ -0,0 +1,8 @@
+valor_tentativas = 0
+
+while True:
+    valor_tentativas += 1
+    if valor_tentativas == 5:
+        print("final")
+        break
+    print(valor_tentativas)
